2da30a6f7721bfc66135e531b61a0316622e66b5
Benjamin Jesuiter <bjesuiter@gmail.com>
2015-12-01 21:07:41 +0100

    added library file and main codefile

---
+++ lib/dart_switch.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+
+
+/// The dart_switch library
+///
+/// This library makes long "if - if else" blocks fancier
+library dart_switch;
+
+export 'src/switch.dart';
+
