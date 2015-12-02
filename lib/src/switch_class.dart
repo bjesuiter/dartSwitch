724a4ee55815f7d8866deff73f689bd9d4359a40
Benjamin Jesuiter <bjesuiter@gmail.com>
2015-12-02 22:55:38 +0100

    simplified Default handling in switch_class.dart and added change of status variable indicating multiple default blocks

--- lib/src/switch_class.dart
+++ lib/src/switch_class.dart
@@ -1,18 +1,37 @@
 // Copyright (c) 2015, Benjamin Jesuiter. All rights reserved. Use of this source code
+
 // is governed by an Apache2 license that can be found in the LICENSE file.
 
 library dart_switch.switch_class;
 
 typedef bool Condition();
 
-SwitchClass Switch = new SwitchClass();
+class Switch {
 
-class SwitchClass {
+  ///checks whether any case path was executed or not
+  bool _anyCaseExecuted = false;
+
+  ///prohibits double assignment of default clause
+  bool _defaultAssigned = false;
+
   void Case(condition, Function execute) {
     if (condition is Condition && condition()) {
       execute();
+      _anyCaseExecuted = true;
     } else if (condition is bool && condition) {
       execute();
+      _anyCaseExecuted = true;
     }
   }
+
+  void Default(Function execute) {
+    if (!_anyCaseExecuted && !_defaultAssigned) {
+      execute();
+      _defaultAssigned = true;
+    } else if (_defaultAssigned) {
+      throw new Exception(
+          "Only one default path allowed per new Switch.run()");
+    }
+  }
+
 }
